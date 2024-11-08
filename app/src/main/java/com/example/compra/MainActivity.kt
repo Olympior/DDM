765f6dc5e443ce56486671de97d6e27760178cae
Olympior <victorolympio5@outlook.com.br>
2024-11-08 02:00:40 -0300

    Activity's

--- app/src/main/java/com/example/compra/MainActivity.kt
+++ app/src/main/java/com/example/compra/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.compra
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,14 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Configurando o botão de login (button2)
+        val btnLogin = findViewById<Button>(R.id.button2)
+        btnLogin.setOnClickListener {
+            // Ir para a tela de comparador
+            val intent = Intent(this, ComparadorActivity::class.java)
+            startActivity(intent)
         }
     }
 }
